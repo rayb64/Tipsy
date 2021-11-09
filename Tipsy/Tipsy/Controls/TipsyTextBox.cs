40aa5332828bc69408806647fe95f443d4c37e77
Ray Birklid <rayb64@rocketmail.com>
2021-11-09 08:32:16 -0800

    Introducing a text box control that selects all within itself when it receives keyboard focus.

---
+++ Tipsy/Tipsy/Controls/TipsyTextBox.cs
@@ -0,0 +1,35 @@
+﻿// <Copyright>Copyright (c) Birklid Software. All rights reserved.</Copyright>
+// <File>TipsyTextBox.cs</File>
+// <Company>Birklid Software</Company>
+// <Author>Ray Birklid</Author>
+// <Email>Ray.Birklid@gmail.com</Email>
+// <Date>11/9/2021 8:26:17 AM</Date>
+namespace Com.Gmail.Birklid.Ray.Tipsy.Controls
+{
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    public class TipsyTextBox : TextBox
+    {
+        #region Creation
+
+        public TipsyTextBox()
+            : base()
+        {
+        }
+
+        #endregion Creation
+
+        #region Protected Members
+
+        protected override void OnGotKeyboardFocus(
+            KeyboardFocusChangedEventArgs e)
+        {
+            base.OnGotKeyboardFocus(e);
+            SelectAll();
+            e.Handled = true;
+        }
+
+        #endregion Protected Members
+    }
+}
